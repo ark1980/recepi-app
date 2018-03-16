052c8ef3246224fa1400a0960b3d56005fe7fffe
Ali <ar.k@outlook.com>
2018-03-15 23:01:58 -0700

    trnasfer all datas from the file into app state for passing as props

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,32 @@ class App extends Component {
   constructor() {
     super()
 
-    this.state = { recipes }
+    this.state = {
+      recipes: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "https://de.rc-cdn.community.thermomix.com/recipeimage/5ip58672-658a1-584620-cfcd2-gz8xg48b/332f3f88-d48f-4c58-a0e2-11f82ecfaf24/main/spaghetti-bolognese.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "https://amandascookin.com/wp-content/uploads/2017/02/nice-cream-milkshakes-H.jpg"
+        },
+        { 
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/12/11/0/FNK_All-the-Avocado-Toast_s4x3.jpg.rend.hgtvcom.616.462.suffix/1450059496131.jpeg"
+        }
+      ],
+      nextId: 3
+    }
   }
 
   render() {
